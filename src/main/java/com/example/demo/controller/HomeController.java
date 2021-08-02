d2f4ed290a3fe9e3e53817bb057efc84f0699c94
yamas <yamas@192.168.11.4>
2021-08-02 22:16:32 +0900

    initialize project

---
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -0,0 +1,17 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/")
+@Controller
+public class HomeController {
+  @GetMapping
+  public String open(Model model) {
+    String str = "Hello World";
+    model.addAttribute("value", str);
+    return "home";
+  }
+}
